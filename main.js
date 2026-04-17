d84fa6e53414219ac5431808a5e8a7683296d5b8
vistro-9e-ay2526 <vistro.pi@obmontessori.edu.ph>
2026-04-17 10:39:46 +0800

    Initial login page setup
    Car Counter - Loop

---
+++ main.js
@@ -0,0 +1,10 @@
+function enterGarage() {
+    let carCounter = 0;
+    const maxCars = 20;
+
+    while (carCounter < maxCars) {
+        carCounter += 1;
+        window.alert("Car Number "+ carCounter +" is entering");
+}
+        window.alert("Garage is full");
+};
