63be82bf89e3ce30deabd2b350c6b27698d5d25c
vistro-9e-ay2526 <vistro.pi@obmontessori.edu.ph>
2026-04-17 11:09:40 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-function enterGarage() {
-    let carCounter = 0;
-    const maxCars = 20;
+let carCounter = 0;
+const maxCars = 20;
 
+function enterGarage() {
     while (carCounter < maxCars) {
         carCounter += 1;
         window.alert("Car Number "+ carCounter +" is entering");
+        break;
 }
+    if (carCounter >= maxCars) {
         window.alert("Garage is full");
-};
+    }
+}
